d060bf49b64478f3e5ac1a1566efee7fc547633a
zippo-yap <zippo-yap@gmail.com>
2016-04-16 16:54:20 +1200

    Tidy indentation and unused code

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
+// Run the code after the page has loaded
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('keyup', keysPressed);
 
+  // Create an array of the tracks for each player.
+  var player1 = document.querySelector('.one').children;
+  var player2 = document.querySelector('.two').children;
 
-// Create a function to advance a particular player1Grid's position by one each time their key is pressed.
-// You should set player as a parameter e.g.
-var game = document.querySelector('.game');
-var player = game.querySelectorAll('.players'); //tracks all players
-// Create an array of the grids for each player.
-
-//for (var i = 0; i >players.length; i++) {
-//  player[i].addEventListener('click',function)
-//}
-
-var player1 = document.querySelector('.one').children
-var player2 = document.querySelector('.two').children
-
-// Function to Update player position
-function updatePlayerPosition(player) {
-  for (var i = 0; i < player.length; i++){
-    if (player[i].className === "active" && player[i].nextElementSibling !== null) {
-    player[i].className = '';
-    player[i].nextElementSibling.className = "active";
-    break;
+  // Function to Update player position
+  function updatePlayerPosition (player) {
+    for (var i = 0; i < player.length; i++) {
+      if (player[i].className === 'active' && player[i].nextElementSibling !== null) {
+        player[i].className = '';
+        player[i].nextElementSibling.className = 'active';
+        break
+      }
     }
   }
-} // <-------Close function
 
-document.addEventListener('keyup', keysPressed)
+  function keysPressed (event) {
+    // When M is pressed
+    if (event.which === 90) {
+      updatePlayerPosition(player1);
+    }
+    // When Z is pressed
+    else if (event.which === 77) {
+      updatePlayerPosition(player2);
+    }
+  }
+})
 
-function keysPressed(event){
-  if (event.which == 90){ // M
-    updatePlayerPosition(player1)
-  }
-  else if (event.which == 77){ //Z
-    updatePlayerPosition(player2)
-  }
-}
