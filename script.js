731b7e0da7a1bf95d569352255f205d575696457
zippo-yap <zippo-yap@gmail.com>
2016-04-16 16:35:13 +1200

    Consolidate player functions into one function

--- script.js
+++ script.js
@@ -13,23 +13,13 @@ var player = game.querySelectorAll('.players'); //tracks all players
 var player1 = document.querySelector('.one').children
 var player2 = document.querySelector('.two').children
 
- var i = 0; // Player 1 Position
- var j = 0; // Player 2 Position
-
-
- function updatePlayerPosition (event,player){
-  if (event.which == 90){
-    if (player1[i].className === "active" && player1[i].nextElementSibling !== null) {
-      player1[i].className = "";
-      player1[i].nextElementSibling.className = "active";
-      i++;
-    }
-  } // <--- Close first if
-  if (event.which == 77){
-    if(player2[j].className == "active" && player2[j].nextElementSibling !== null){ 
-      player2[j].className = "";
-      player2[j].nextElementSibling.className = "active";
-      j++;
+// Function to Update player position
+function updatePlayerPosition(player) {
+  for (var i = 0; i < player.length; i++){
+    if (player[i].className === "active" && player[i].nextElementSibling !== null) {
+    player[i].className = '';
+    player[i].nextElementSibling.className = "active";
+    break;
     }
   }
 } // <-------Close function
@@ -39,6 +29,3 @@ document.addEventListener('keyup', updatePlayerPosition)
 
 // Z = 90
 // M = 77
-
-var p1keys = 90;
-var p2keys = 77;
