2d0c4519b50e348b43de99d3b511f4cbe16e3d99
zippo-yap <zippo-yap@gmail.com>
2016-04-17 23:30:03 +1200

    Add winning and finish states

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 // Run the code after the page has loaded
 document.addEventListener('DOMContentLoaded', function () {
-  document.addEventListener('keyup', keysPressed);
 
   // Create an array of the tracks for each player.
   var player1 = document.querySelector('.one').children;
   var player2 = document.querySelector('.two').children;
+ 
+  // Prompt the player if they're ready to play
+  var playerReady = confirm('Are you ready to play?');
+
+  // Activate keys to play game
+  function play () {
+    if (playerReady){
+      document.addEventListener('keyup', keysPressed);
+    }
+  }
+
+  // Do you want to play again?
+  function endGame () {
+    var playAgain = confirm('Do you want to play again?');
+    if (playAgain){
+      console.log("Play Again");
+      // Call function to reset game
+      reset();
+    }
+    else {
+      console.log('Nope')
+    }
+  }
+
+  // Reset the game
+  function reset () {
+    for (var i = 0; i < player1.length; i++){
+      if (player1[i].className === 'active') {
+        player1[i].className = '';
+      }
+      if (player2[i].className === 'active') {
+        player2[i].className = '';
+      }
+    }
+    player1[0].className = 'active'
+    player2[0].className = 'active'
+    play();
+  }
+
+  play();
+
 
   // Function to Update player position
   function updatePlayerPosition (player) {
@@ -13,7 +53,9 @@ document.addEventListener('DOMContentLoaded', function () {
         player[i].className = '';
         player[i].nextElementSibling.className = 'active';
         if (player[i].nextElementSibling === player[player.length -1]) {
-          alert ("Winner Winner Winner") // Insert Finish function or alert
+          document.removeEventListener('keyup', keysPressed);
+          // Call a function that resets the game
+          endGame();
         }
         else {
              break;
@@ -30,6 +72,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // When Z is pressed
     else if (event.which === 77) {
       updatePlayerPosition(player2);
-    }
-  }
-})
+    } //Close keysPressed
+  } // Close event listener
+}) // Close document loader
